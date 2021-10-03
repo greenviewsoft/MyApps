20d89f369b13889ba3fed33d8ec0cb630712702b
Your Name <you@example.com>
2021-10-04 01:40:28 +0800

    thirds comment

---
+++ android/app/src/main/kotlin/com/example/myapps/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.myapps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return 
+     
+     
+        AppBar(
+      title: Text(
+        'Bottom Navy Bar',
+        style: TextStyle(color: Colors.black),
+      ),
+      backgroundColor: Colors.white,
+    
+    
+  }
+}
